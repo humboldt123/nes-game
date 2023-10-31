d2014a055617a282f1c6f0f74f19469a6928999c
vish <38266782+humboldt123@users.noreply.github.com>
2023-10-31 12:35:02 -0400

    cooler way to write text to screen

--- src/game.c
+++ src/game.c
@@ -1,31 +1,42 @@
 #include <neslib/neslib.h>
 #include <stdint.h>
 
-#pragma bss-name (push,"ZEROPAGE")
-#pragma data-name (push,"ZEROPAGE")
+#pragma bss-name(push,"ZEROPAGE")
+#pragma data-name(push,"ZEROPAGE")
 uint8_t oam_off;
 #pragma data-name(pop)
-#pragma bss-name (pop)
+#pragma bss-name(pop)
 
 // form feed is 0x0C which is a heart in this palette
-unsigned char text[] = "vish was here \f";
+unsigned char text[] = {
+	// screen is 32 x 30 tiles
+	MSB(NTADR_A(9,14))|NT_UPD_HORZ, // where to write, repeat horizontally
+	LSB(NTADR_A(9,14)),
+	15, // length
+	'v','i','s','h',' ','w','a','s',' ','h','e','r','e',' ','\f',
+	NT_UPD_EOF // must end in eof
+};
 const uint8_t palette[] = { 0x0F, 0x06, 0x15, 0x36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+// loop variable
+unsigned char i;
 
 void main (void) {
 	// screen off
 	ppu_off();
 	pal_bg(palette);
 
-	// screen is 32 x 30 tiles
-	vram_adr(NTADR_A(10,14)); 
+	ppu_on_all(); // turn on the screen (activate ppu)
 	
-	// Write text to the starting point
-	vram_write(text, sizeof(text));
+	set_vram_update(text);
 
-	// Activate PPU
-	ppu_on_all();
+	ppu_wait_nmi(); // we will be writing on the next frame 
+
+	set_vram_update(NULL); // stop writing it over and over
 
 	while (1) {
-		/* Game Loop */
+		// write text to the starting point
+		
+		
 	}
 }
